6c6e6f960dd9826b988750484a936909410826b5
ilham afemi <ilhamafemi@gmail.com>
2019-03-05 14:12:09 +0700

    update admin

--- OvertimeRequest/src/views/AdminView.java
+++ OvertimeRequest/src/views/AdminView.java
@@ -57,29 +57,36 @@ public class AdminView extends javax.swing.JInternalFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        lblSearch = new javax.swing.JLabel();
-        tfSearch = new javax.swing.JTextField();
+        tfRole = new javax.swing.JTextField();
+        jLabel5 = new javax.swing.JLabel();
         btFind = new javax.swing.JButton();
         btSave = new javax.swing.JButton();
+        jLabel2 = new javax.swing.JLabel();
         btDelete = new javax.swing.JButton();
+        tfSearch = new javax.swing.JTextField();
         jScrollPane1 = new javax.swing.JScrollPane();
         tbUser = new javax.swing.JTable();
-        tfId = new javax.swing.JTextField();
-        jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
+        btClear = new javax.swing.JButton();
+        lblSearch = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
+        tfId = new javax.swing.JTextField();
         tfName = new javax.swing.JTextField();
+        jLabel1 = new javax.swing.JLabel();
         tfJob = new javax.swing.JTextField();
-        tfRole = new javax.swing.JTextField();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
         jMenuItem2 = new javax.swing.JMenuItem();
         jMenuItem3 = new javax.swing.JMenuItem();
         jMenuItem4 = new javax.swing.JMenuItem();
 
-        lblSearch.setText("Search User");
+        setBackground(new java.awt.Color(199, 220, 236));
 
+        jLabel5.setFont(new java.awt.Font("Courier New", 1, 18)); // NOI18N
+        jLabel5.setText("User Access Setting");
+
+        btFind.setBackground(new java.awt.Color(128, 137, 149));
+        btFind.setFont(new java.awt.Font("Courier New", 0, 11)); // NOI18N
         btFind.setText("Find");
         btFind.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -87,6 +94,8 @@ public class AdminView extends javax.swing.JInternalFrame {
             }
         });
 
+        btSave.setBackground(new java.awt.Color(128, 137, 149));
+        btSave.setFont(new java.awt.Font("Courier New", 0, 11)); // NOI18N
         btSave.setText("Save");
         btSave.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -94,7 +103,17 @@ public class AdminView extends javax.swing.JInternalFrame {
             }
         });
 
+        jLabel2.setFont(new java.awt.Font("Courier New", 0, 11)); // NOI18N
+        jLabel2.setText("Name");
+
+        btDelete.setBackground(new java.awt.Color(128, 137, 149));
+        btDelete.setFont(new java.awt.Font("Courier New", 0, 11)); // NOI18N
         btDelete.setText("Delete");
+        btDelete.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btDeleteActionPerformed(evt);
+            }
+        });
 
         tbUser.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -114,15 +133,25 @@ public class AdminView extends javax.swing.JInternalFrame {
         });
         jScrollPane1.setViewportView(tbUser);
 
-        tfId.setEditable(false);
-
-        jLabel1.setText("Id");
-
-        jLabel2.setText("Name");
-
+        jLabel3.setFont(new java.awt.Font("Courier New", 0, 11)); // NOI18N
         jLabel3.setText("Job");
 
+        btClear.setBackground(new java.awt.Color(128, 137, 149));
+        btClear.setText("Clear");
+        btClear.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btClearActionPerformed(evt);
+            }
+        });
+
+        lblSearch.setFont(new java.awt.Font("Courier New", 1, 11)); // NOI18N
+        lblSearch.setText("Search User");
+
+        jLabel4.setFont(new java.awt.Font("Courier New", 0, 11)); // NOI18N
         jLabel4.setText("Role");
+
+        jLabel1.setFont(new java.awt.Font("Courier New", 0, 11)); // NOI18N
+        jLabel1.setText("Id");
 
         jMenu1.setText("Menu");
 
@@ -150,39 +179,43 @@ public class AdminView extends javax.swing.JInternalFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(lblSearch)
+                            .addComponent(jLabel5))
+                        .addGap(0, 0, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jLabel1)
                             .addComponent(jLabel2)
                             .addComponent(jLabel3)
                             .addComponent(jLabel4))
                         .addGap(18, 18, 18)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(btSave, javax.swing.GroupLayout.PREFERRED_SIZE, 82, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(btDelete, javax.swing.GroupLayout.PREFERRED_SIZE, 86, javax.swing.GroupLayout.PREFERRED_SIZE))
                             .addComponent(tfRole)
                             .addComponent(tfJob)
                             .addComponent(tfName)
                             .addComponent(tfId)))
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(lblSearch)
-                        .addGap(0, 0, Short.MAX_VALUE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(tfSearch, javax.swing.GroupLayout.PREFERRED_SIZE, 101, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 17, Short.MAX_VALUE)
-                        .addComponent(btFind))
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addGap(0, 0, Short.MAX_VALUE)
-                        .addComponent(btSave, javax.swing.GroupLayout.PREFERRED_SIZE, 57, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(btDelete)))
+                        .addGap(4, 4, 4)
+                        .addComponent(tfSearch)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(btClear, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(btFind, javax.swing.GroupLayout.DEFAULT_SIZE, 86, Short.MAX_VALUE))))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
                     .addGroup(layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 258, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(17, 17, 17)
+                        .addComponent(jLabel5)
+                        .addGap(18, 18, 18)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(jLabel1)
                             .addComponent(tfId, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
@@ -204,11 +237,14 @@ public class AdminView extends javax.swing.JInternalFrame {
                             .addComponent(btDelete))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(lblSearch)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGap(9, 9, 9)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(tfSearch, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(btFind))))
-                .addContainerGap(80, Short.MAX_VALUE))
+                            .addComponent(btFind))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(btClear)
+                        .addGap(0, 79, Short.MAX_VALUE)))
+                .addContainerGap())
         );
 
         pack();
@@ -219,6 +255,31 @@ public class AdminView extends javax.swing.JInternalFrame {
         tableData(rci.search(key));
     }//GEN-LAST:event_btFindActionPerformed
 
+    private void btSaveActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btSaveActionPerformed
+        // TODO add your handling code here:
+        if (tfSearch.equals("")) JOptionPane.showMessageDialog(null, "Fill Empty Data Please !");
+        else
+        {
+            try {
+                int reply = JOptionPane.showConfirmDialog(null,
+                    "Are sure save the data?", "Confirmation", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE
+                );
+                if (reply == JOptionPane.YES_OPTION) {
+                    JOptionPane.showMessageDialog(null, rci.save(tfId.getText(), tfName.getText(), tfJob.getText(), tfRole.getText()));
+                    tfSearch.setText("");
+                    tableData(rci.getAll());
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        //        tableData(role);
+    }//GEN-LAST:event_btSaveActionPerformed
+
+    private void btDeleteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btDeleteActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btDeleteActionPerformed
+
     private void tbUserMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_tbUserMouseClicked
         // TODO add your handling code here:
         tfId.setText(tbUser.getValueAt(tbUser.getSelectedRow(), 1).toString());
@@ -227,29 +288,20 @@ public class AdminView extends javax.swing.JInternalFrame {
         tfRole.setText(tbUser.getValueAt(tbUser.getSelectedRow(), 4).toString());
     }//GEN-LAST:event_tbUserMouseClicked
 
-    private void btSaveActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btSaveActionPerformed
+    private void btClearActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btClearActionPerformed
         // TODO add your handling code here:
-         if (tfSearch.equals("")) JOptionPane.showMessageDialog(null, "Fill Empty Data Please !");
-        else 
-        {
-            try {
-                int reply = JOptionPane.showConfirmDialog(null,
-                        "Are You Sure To Delete?", "Confirmation", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE
-                );
-                if (reply == JOptionPane.YES_OPTION) {
-//                    JOptionPane.showMessageDialog(null, rci.delete(tfSearch.getText()));
-                    tfSearch.setText("");
-                    tableData(rci.getAll());
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-//        tableData(role);
-    }//GEN-LAST:event_btSaveActionPerformed
+        tfId.setText("");
+        tfJob.setText("");
+        tfName.setText("");
+        tfRole.setText("");
+        tfSearch.setText("");
+        tfId.setEnabled(true);
+        tableData(rci.getAll());
+    }//GEN-LAST:event_btClearActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btClear;
     private javax.swing.JButton btDelete;
     private javax.swing.JButton btFind;
     private javax.swing.JButton btSave;
@@ -257,6 +309,7 @@ public class AdminView extends javax.swing.JInternalFrame {
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
     private javax.swing.JMenu jMenu1;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JMenuItem jMenuItem2;
